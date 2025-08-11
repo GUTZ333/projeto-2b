ede10f4e42e3bec261b6c318ff276ab5aea26464
GUTZ333 <170575706+GUTZ333@users.noreply.github.com>
2025-08-11 08:05:06 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+     <main>
+      <header>
+        
+      </header>
+      <section className="produtos">
+
+      </section>
+      <section className="produto-destaque">
+
+      </section>
+      <footer>
+        
+      </footer>
+     </main> 
+    </>
+  );
+}
+
+export default App;
