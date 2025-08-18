cbfa0986c2c40bb89f610e159acf30d578c49b44
GUTZ333 <170575706+GUTZ333@users.noreply.github.com>
2025-08-18 08:01:17 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import "./App.css";
 function App() {
   return (
     <>
-     <main>
-      <header>
-        
-      </header>
-      <section className="produtos">
-
-      </section>
-      <section className="produto-destaque">
-
-      </section>
-      <footer>
-        
-      </footer>
-     </main> 
+      <main>
+        <header></header>
+        <section className="produtos"></section>
+        <section className="produto-destaque">
+          {/* Produto 1 */}
+          <div className="card">
+            <h2>Tenis Nike</h2>
+            <p className="desc">Um lindo tenis da Nike</p>
+            <p className="preco">R$ 699,90</p>
+            <div className="avaliacao">★ ★ ★ ★ ☆</div>
+          </div>
+        </section>
+        <footer></footer>
+      </main>
     </>
   );
 }
