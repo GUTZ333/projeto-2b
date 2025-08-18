a0054c3e307fc9dfaf6ba31f199ec3aa97defac3
GUTZ333 <170575706+GUTZ333@users.noreply.github.com>
2025-08-18 10:55:35 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import "./App.css";
+import TenisNikeIcon from "./assets/tenis-nike.webp";
 
 function App() {
   return (
     <>
       <main>
         <header></header>
-        <section className="produtos"></section>
-        <section className="produto-destaque">
+        <section className="produtos">
           {/* Produto 1 */}
           <div className="card">
             <h2>Tenis Nike</h2>
             <p className="desc">Um lindo tenis da Nike</p>
             <p className="preco">R$ 699,90</p>
             <div className="avaliacao">★ ★ ★ ★ ☆</div>
+            <img src={TenisNikeIcon} alt="tenis nike" className="img-prod" />
           </div>
         </section>
+        <section className="produto-destaque"></section>
         <footer></footer>
       </main>
     </>
