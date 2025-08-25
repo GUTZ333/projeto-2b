4c6100b588a46793c8c40acf363701fbdc6e28e0
GUTZ333 <170575706+GUTZ333@users.noreply.github.com>
2025-08-25 10:55:04 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,38 @@ function App() {
         <header></header>
         <section className="produtos">
           {/* Produto 1 */}
-          <div className="card">
-            <img src={TenisNikeIcon} alt="tenis nike" className="img-prod" />
-            <h2>Tenis Nike</h2>
-            <p className="desc">Um lindo tenis da Nike</p>
-            <p className="preco">R$ 699,90</p>
-            <div className="avaliacao">★ ★ ★ ★ ☆</div>
-            <div className="off">-15% </div>
-          </div>
+          <a className="link-card" href="#">
+            <div className="card">
+              <img src={TenisNikeIcon} alt="tenis nike" className="img-prod" />
+              <h2>Tenis Nike</h2>
+              <p className="desc">Um lindo tenis da Nike</p>
+              <p className="preco">R$ 699,90</p>
+              <div className="avaliacao">★ ★ ★ ★ ☆</div>
+              <div className="off">-15% </div>
+            </div>
+          </a>
+          {/* Produto 2 */}
+          <a className="link-card" href="#">
+            <div className="card">
+              <img src={TenisNikeIcon} alt="tenis nike" className="img-prod" />
+              <h2>Tenis Nike</h2>
+              <p className="desc">Um lindo tenis da Nike</p>
+              <p className="preco">R$ 699,90</p>
+              <div className="avaliacao">★ ★ ★ ★ ☆</div>
+              <div className="off">-15% </div>
+            </div>
+          </a>
+          {/*   Produto 3 */}
+          <a className="link-card" href="#">
+            <div className="card">
+              <img src={TenisNikeIcon} alt="tenis nike" className="img-prod" />
+              <h2>Tenis Nike</h2>
+              <p className="desc">Um lindo tenis da Nike</p>
+              <p className="preco">R$ 699,90</p>
+              <div className="avaliacao">★ ★ ★ ★ ☆</div>
+              <div className="off">-15% </div>
+            </div>
+          </a>
           
         </section>
         <section className="produto-destaque"></section>
