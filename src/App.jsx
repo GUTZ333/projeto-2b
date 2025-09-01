02b09374cdb7fb7a40bb5ca8874482b6055928a0
GUTZ333 <170575706+GUTZ333@users.noreply.github.com>
2025-09-01 08:03:58 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
 import TenisNikeIcon from "./assets/tenis-nike.webp";
+import UniformeFlamengo from "./assets/uniforme-flamengo-original-2025.webp"
+import TenisFlamengo from "./assets/tenis-flamengo.webp"
+import MeiaFlamengo from "./assets/meia-flamengo.webp"
 
 function App() {
   return (
@@ -10,34 +13,34 @@ function App() {
           {/* Produto 1 */}
           <a className="link-card" href="#">
             <div className="card">
-              <img src={TenisNikeIcon} alt="tenis nike" className="img-prod" />
-              <h2>Tenis Nike</h2>
-              <p className="desc">Um lindo tenis da Nike</p>
-              <p className="preco">R$ 699,90</p>
-              <div className="avaliacao">★ ★ ★ ★ ☆</div>
-              <div className="off">-15% </div>
+              <img src={TenisFlamengo} alt="tenis nike" className="img-prod" />
+              <h2>Tenis Flamengo</h2>
+              <p className="desc">Tenis do clube Flamengo</p>
+              <p className="preco">R$ 60,90</p>
+              <div className="avaliacao"><span>★ ★ ★ ★ ☆</span>(336)</div>
+              <div className="off">-20% </div>
             </div>
           </a>
           {/* Produto 2 */}
           <a className="link-card" href="#">
             <div className="card">
-              <img src={TenisNikeIcon} alt="tenis nike" className="img-prod" />
-              <h2>Tenis Nike</h2>
-              <p className="desc">Um lindo tenis da Nike</p>
-              <p className="preco">R$ 699,90</p>
-              <div className="avaliacao">★ ★ ★ ★ ☆</div>
-              <div className="off">-15% </div>
+              <img src={UniformeFlamengo} alt="tenis nike" className="img-prod" />
+              <h2>Uniforme Flamengo</h2>
+              <p className="desc">Uniforme original do Flamengo original de 2025 com patrocínio da Fla.Bet</p>
+              <p className="preco">R$ 475,58</p>
+              <div className="avaliacao"><span>★ ★ ★ ★ ★ </span>(234)</div>
+              <div className="off">-20% </div>
             </div>
           </a>
           {/*   Produto 3 */}
           <a className="link-card" href="#">
             <div className="card">
-              <img src={TenisNikeIcon} alt="tenis nike" className="img-prod" />
-              <h2>Tenis Nike</h2>
-              <p className="desc">Um lindo tenis da Nike</p>
-              <p className="preco">R$ 699,90</p>
-              <div className="avaliacao">★ ★ ★ ★ ☆</div>
-              <div className="off">-15% </div>
+              <img src={MeiaFlamengo} alt="tenis nike" className="img-prod" />
+              <h2>Meia do Flamengo</h2>
+              <p className="desc">Meia do Clube Flamengo</p>
+              <p className="preco">R$ 40,90</p>
+              <div className="avaliacao"><span>★ ★ ★ ★ ★</span>(178)</div>
+              <div className="off">-10% </div>
             </div>
           </a>
           
